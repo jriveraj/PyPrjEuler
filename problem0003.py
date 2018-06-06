e256ffadeb54e3b3f43fbb8e70994fbaa6a73992
JRivera <jesusrivera00@gmail.com>
2018-06-06 15:02:19 -0500

    Modified Prob 3 & 4

--- problem0003.py
+++ problem0003.py
@@ -3,6 +3,32 @@
 #What is the largest prime factor of the number 600851475143 ?
 
 
+#nTarget = 600851475143
+nTarget = 13195
+
+nPrimes = []
+nPrimes.append( 2 )
+
+def GeneratePrimeList():
+    nCount = 3
+    while nCount <= nTarget:
+        nCountPrimes = 0
+        bIsPrime = True
+        while nCountPrimes < len( nPrimes ) and bIsPrime:
+            if nCount % nPrimes[ nCountPrimes ] == 0:
+                bIsPrime = False
+            nCountPrimes = nCountPrimes + 1
+        if bIsPrime:
+            nPrimes.append( nCount )
+        nCount  = nCount + 2
+
+GeneratePrimeList()
+
+nCount = len( nPrimes )
+
 nTarget = 600851475143
 
-#this is a new line added
+while nCount > 0:
+    nCount = nCount - 1
+    if nTarget % nPrimes[ nCount ] == 0:
+        print( nPrimes[ nCount ] )
